90577b2f76bb7a95ef25e549406e34734d4965db
Denis Hananein <i@zloylos.me>
2015-08-26 13:12:38 +0300

    update plugins interface

--- interfaces/IPlugin.js
+++ interfaces/IPlugin.js
@@ -6,25 +6,38 @@
  */
 var plugin = {
     /**
-     * Type of messages, when plugin should be run.
+     * Type of messages, when a plugin should be run.
      * @field
      * @type {String}
      */
     type: 'TEXT',
 
     /**
+     * Weight of a plugin.
+     * @field
+     * @type {Number}
+     */
+     weight: 0.2,
+
+    /**
      * @param {Object} info
      * @param {TelegramMessage} info.message
-     * @param {Object} info.data Information from analyzer.
+     * @param {Object} info.data Information from a message analyzer.
      * @returns {Boolean}
      *
      * @example
      * // If this is text message.
-     * is: function (message) {
-     *     return typeof message.text === 'string';
+     * test: function (info) {
+     *     return typeof info.message.text === 'string';
+     * }
+     *
+     * @example
+     * // If is the `weather` command (`/weather`).
+     * test: function (info) {
+     *     return info.data.command === 'weather';
      * }
      */
-    is: function (information) {},
+    test: function (information) {},
 
     /**
      * @param {Object} info
@@ -32,6 +45,7 @@ var plugin = {
      * @param {Object} info.data Information from analyzer.
      * @param {Object} [info.user] User object.
      * @param {TelegramBot} bot
+     * @returns {Promise|any} Promise of any object if you don't use async operations.
      */
     handler: function (info, bot) {}
 };
